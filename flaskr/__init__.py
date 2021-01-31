d0636b0c3cba8ec145c118b5cfbbd664d601ff89
JC <node.zerocool@gmail.com>
2021-01-30 21:36:18 -0600

    struct

---
+++ flaskr/__init__.py
@@ -0,0 +1,18 @@
+
+from flask import Flask
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flasker.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config is passed in
+        app.config.from_mapping(test_config)
+    
